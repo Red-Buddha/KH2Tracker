45ec1c51419304066d1063e3873977472e83bfcb
Trevor Luckey <trevorluckey@gmail.com>
2021-01-30 17:05:11 -0800

    Change log filename

--- KhTracker/App.xaml.cs
+++ KhTracker/App.xaml.cs
@@ -17,7 +17,7 @@ namespace KhTracker
 
         App()
         {
-            logger = new Log("test.txt");
+            logger = new Log("log.txt");
         }
 
         private void App_Exit(object sender, ExitEventArgs e)
